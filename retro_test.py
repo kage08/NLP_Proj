91e256cc7f14d5876fb38acc9897bee1480f8bd7
kage08 <harshavardhan864.hk@gmail.com>
2019-05-08 05:21:12 +0530

    Retrofit reuylts

--- retro_test.py
+++ retro_test.py
@@ -3,24 +3,48 @@ from web.datasets.similarity import fetch_WS353
 from retrofitting.retrofit import retrofit, retrofit_wnsim
 from web.evaluate import evaluate_similarity, evaluate_on_all
 from evaluation.funs import wordsim_eval
+from embed import EmbedModel
+from copy import deepcopy
+import numpy as np
 
-data = fetch_WS353()
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
+
+methods = ['simple','path', 'lch','jcn','lin', 'wup']
+hops = [1,2,3]
+res_file = open('data/results.csv','a')
+
 
 emb = pickle.load(open('data/glove.pkl','rb'))
-ds = pickle.load(open('data/glove_neigh.pkl','rb'))
+ds_ = pickle.load(open('data/glove_neigh.pkl','rb'))
 
-for w in ds.keys():
-    ds[w][0].extend(ds[w][1])
-    ds[w][0].extend(ds[w][2])
-    ds[w] = ds[w][0]
-
-
-from embed import EmbedModel
-
-emb_lch=retrofit_wnsim(emb, ds, 1,'lch') 
-
-ans = EmbedModel(retrofit_wnsim(emb,ds, 10,'lch'))
-#emb = EmbedModel(emb)
-
-#print(wordsim_eval(emb))
-print(wordsim_eval(ans))
+for m in methods:
+    for h in hops:
+        name = '_'.join(['glove',str(h),m])
+        print('Doing ',name)
+        ds = deepcopy(ds_)
+        for w in ds.keys():
+            for i in range(1,h):
+                ds[w][0].extend(ds[w][h])
+            ds[w] = ds[w][0]
+        
+        if m == 'simple':
+            new_embed = retrofit(emb, ds, 20)
+            new_embed = (EmbedModel(new_embed), None)
+        else:
+            new_embed = retrofit_wnsim(emb, ds, 20, m)
+            new_embed = (EmbedModel(new_embed[0]), new_embed[1])
+        print('Calculating Results ...')
+        results = evaluate_on_all(new_embed[0].vocab)
+        print(results)
+        line = [name]
+        line.extend([str(x) for x in np.array(results)[0]])
+        res_file.write(','.join(line))
+        res_file.write('\n')
+        res_file.flush()
+        with open('data/'+name+'.pkl','wb') as fl:
+            pickle.dump(new_embed, fl)
+        del ds
+        del new_embed
+        print('\n#################################################')
